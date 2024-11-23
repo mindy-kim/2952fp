7acac3a62be2376bbe6edfa49af4f4f8f2ce6b83
mindy-kim <mindy_kim@brown.edu>
2024-11-23 18:02:47 -0500

    updated transformer

--- model.py
+++ model.py
@@ -11,6 +11,7 @@ class Transformer(nn.Module):
         self.timesteps = timesteps
 
         self.self_attn = MultiHeadAttention(size, nhead)
+        self.feed_forward = PositionwiseFeedForward(size, dim_ff, dropout)
         self.sublayer1 = SublayerConnection(size, dropout)
         self.sublayer2 = SublayerConnection(size, dropout)
 
@@ -20,49 +21,95 @@ class Transformer(nn.Module):
         for _ in range(self.timesteps):
             x = self.sublayer1(x, lambda x: self.self_attn(x, x, x))
             x = self.sublayer2(x, self.feed_forward)
-
-        x = self.out(x)
+            x = self.out(x)
 
         return x
 
-class AttentionHead(nn.Module):
-    def __init__(self, input_size, output_size):
-        super(AttentionHead, self).__init__()
-        self.K = nn.Parameter(torch.randn(input_size, output_size))
-        self.Q = nn.Parameter(torch.randn(input_size, output_size))
-        self.V = nn.Parameter(torch.randn(input_size, output_size))
+class LinearSelfAttention(nn.Module):
+    def __init__(self):
+        super(LinearSelfAttention, self).__init__()
 
-    def forward(self, inputs_for_keys, inputs_for_values, inputs_for_queries):
-        K = torch.tensordot(inputs_for_keys, self.K, 1)
-        V = torch.tensordot(inputs_for_values, self.V, 1)
-        Q = torch.tensordot(inputs_for_queries, self.Q, 1)
+    def forward(self, q, k, v):
+        # input is 4 dimension tensor
+        # [batch_size, head, length, d_tensor]
+        k_t = k.transpose(2, 3)
+        score = q @ k_t
+        weighted = score @ v
 
-        scores = torch.matmul(Q, K.T) # double check if this is right
-        weighted = scores @ V
         return weighted
 
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self, emb_size, num_heads):
+    def __init__(self, d_model, d_out, n_head):
         super(MultiHeadAttention, self).__init__()
-        self.num_heads = num_heads
-        self.emb_size = emb_size
-        self.heads = []
+        self.n_head = n_head
+        self.self_attention = LinearSelfAttention()
+        self.w_q = nn.Linear(d_model, d_out)
+        self.w_k = nn.Linear(d_model, d_out - 1)
+        self.w_v = nn.Linear(d_model, d_out - 1)
+        self.w_concat = nn.Linear(d_model, d_model)
 
-        for _ in range(self.num_heads):
-            self.heads.append(AttentionHead(emb_size*self.num_heads, emb_size))
+    def forward(self, q, k, v, is_concat=True):
+        q, k, v = self.w_q(q), self.w_k(k), self.w_v(v)
+        q, k, v = self.split(q), self.split(k), self.split(v)
 
-        # self.combine_heads = nn.Linear(emb_size, emb_size) # is this needed..?
+        out = self.self_attention(q, k, v)
+        
+        if is_concat:
+            out = self.concat(out)
+        else:
+            out = torch.sum(out, dim=1)
 
-    def forward(self, inputs_for_keys, inputs_for_values, inputs_for_queries):
-        out = torch.zeros_like(inputs_for_keys) # don't know if this is right dim
+        out = self.w_concat(out)
 
-        for i in range(self.num_heads):
-            out += self.heads[i](inputs_for_keys, inputs_for_values, inputs_for_queries)
-
-        # out = self.combine_heads(out)
         return out
 
+    def split(self, tensor):
+        """
+        split tensor by number of head
+
+        :param tensor: [batch_size, length, d_model]
+        :return: [batch_size, head, length, d_tensor]
+        """
+        batch_size, length, d_model = tensor.size()
+
+        d_tensor = d_model // self.n_head
+        tensor = tensor.view(batch_size, length, self.n_head, d_tensor).transpose(1, 2)
+        # it is similar with group convolution (split by number of heads)
+
+        return tensor
+
+    def concat(self, tensor):
+        """
+        inverse function of self.split(tensor : torch.Tensor)
+
+        :param tensor: [batch_size, head, length, d_tensor]
+        :return: [batch_size, length, d_model]
+        """
+        batch_size, head, length, d_tensor = tensor.size()
+        d_model = head * d_tensor
+
+        tensor = tensor.transpose(1, 2).contiguous().view(batch_size, length, d_model)
+        return tensor
+
+
+class PositionwiseFeedForward(nn.Module):
+    ''' Implements the two-layer feedforward neural network used in the transformer.'''
+    def __init__(self, d_model: int, d_ff: int, dropout: float = 0.1):
+        """
+        Initializes a PositionwiseFeedForward module
+        """
+        super(PositionwiseFeedForward, self).__init__()
+        self.w_1 = nn.Linear(d_model, d_ff)
+        self.w_2 = nn.Linear(d_ff, d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass of the model. Normalizes the input, applies the sublayer,
+        performs a dropout, and then performs a residual connection.
+        """
+        return self.w_2(self.dropout(self.w_1(x).relu()))
 
 class SublayerConnection(nn.Module):
     '''Applies a residual connection followed by a layer norm to any sublayer'''
@@ -87,21 +134,3 @@ class SublayerConnection(nn.Module):
         performs a dropout, and then performs a residual connection.
         """
         return x + self.dropout(sublayer(self.norm(x)))
-    
-class PositionwiseFeedForward(nn.Module):
-    ''' Implements the two-layer feedforward neural network used in the transformer.'''
-    def __init__(self, d_model: int, d_ff: int, dropout: float = 0.1):
-        """
-        Initializes a PositionwiseFeedForward module
-        """
-        super(PositionwiseFeedForward, self).__init__()
-        self.w_1 = nn.Linear(d_model, d_ff)
-        self.w_2 = nn.Linear(d_ff, d_model)
-        self.dropout = nn.Dropout(dropout)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """
-        Forward pass of the model. Normalizes the input, applies the sublayer,
-        performs a dropout, and then performs a residual connection.
-        """
-        return self.w_2(self.dropout(self.w_1(x).relu()))
