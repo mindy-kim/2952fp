bb1a397bd369002347d3c129c664fd14f2775976
mindy-kim <mindy_kim@brown.edu>
2024-12-14 14:04:55 -0500

    hijacking

--- data.py
+++ data.py
@@ -74,7 +74,6 @@ class MULData(Dataset):
             out_dict['xsF'], out_dict['ysF'], out_dict['weightsF'] = xsF[0], ysF[0], weightsF[0]
             
         return out_dict
-
     
     def sample_df(self, N):
         """
@@ -116,14 +115,82 @@ class MULData(Dataset):
         xs = torch.rand((N, self.C, self.Nx)) * 2.0 - 1.0
         ys = torch.einsum('ijk,ick->icj', weights, xs)
         return xs, ys, weights
+    
+class HData(Dataset):
+    def __init__(self, input_dir):
+        super().__init__()
+        self.input_dir = input_dir
+        if self.input_dir: # check if it is not empty
+            # compile retain and forget tasks from before
+            data = self.compile_data(self.input_dir)
+
+    def compile_data(self, dir):
+        # compile data from saved batches
+        pass
+    
+    def __getitem__(self, idx):
+        out_dict = {}
+
+        xs, ys, weights = self.sample_dr(1) # retain 
+        out_dict['xs'], out_dict['ys'], out_dict['weights'] = xs[0], ys[0], weights[0]
+
+        xsF, ysF, weightsF = self.sample_df(1) # forget
+        out_dict['xsF'], out_dict['ysF'], out_dict['weightsF'] = xsF[0], ysF[0], weightsF[0]
+            
+        return out_dict
+    
+    def sample_df(self, N):
+        """
+        Get N samples from the forget set
+        """
+        tIdx = torch.randint(0, self.Nf, (N,))
+        orig_weights = self.weightsF[tIdx] 
+
+        noise = self.EN * torch.randn_like(orig_weights)
+        while True:
+            mask = F.norm(noise, dim=(-2,-1), keepdim=True) > 1.0
+            if torch.any(mask):
+                noise = torch.where(mask, self.EN * torch.randn_like(orig_weights), noise)
+                continue
+            
+            break
+
+        weights = orig_weights + self.epsW * noise
+        xs = torch.rand((N, self.C, self.Nx)) * 2.0 - 1.0
+        ys = torch.einsum('ijk,ick->icj', weights, xs)
+        return xs, ys, weights
+    
+    def sample_dr(self, N):
+        """
+        Get N samples from the retain set
+        """
+        weights = torch.randn((N, self.Ny, self.Nx))
+        while True:
+            mask = torch.zeros((N,1,1))
+            for i in range(self.Nf):
+                mask = torch.logical_or(F.norm(weights-self.weightsF[i], dim=(-2,-1), keepdim=True) < self.epsW, mask)
+            
+            if torch.any(mask):
+                weights = torch.where(mask, torch.randn((N, self.Ny, self.Nx)), weights)
+                continue
+            
+            break
+            
+        xs = torch.rand((N, self.C, self.Nx)) * 2.0 - 1.0
+        ys = torch.einsum('ijk,ick->icj', weights, xs)
+        return xs, ys, weights
+    
 
 
 if __name__ == '__main__':
+    ''' BASELINE DATA '''
     # start = time.perf_counter()
     # xs, ys, weights = data.sample_df(10000)
     # end = time.perf_counter()
     # print(end - start)
     # print(xs.size(), ys.size(), weights.size())
+
+    ''' UNLEARNING DATA '''
     NUM_TRIALS = 1000
     tot = 0
     for i in range(NUM_TRIALS):
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from utils.utils import instantiate_from_config
 
 from torch.utils.data import DataLoader
 import pytorch_lightning as pl
-from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.callbacks import ModelCheckpoint, Callback
 from pytorch_lightning.loggers import TensorBoardLogger
 
 from models.transformer import Transformer
@@ -39,6 +39,17 @@ def get_parser(**parser_kwargs):
 
     return parser.parse_args()
 
+class SaveInputCallback(Callback):
+    def __init__(self, out_dir):
+        super().__init__()
+        self.out_dir = out_dir
+    
+    def on_train_batch_start(self, trainer, pl_module, batch, batch_idx, dataloader_idx=0):
+        # Save the data for each batch
+        save_path = os.path.join(self.out_dir, f"batch_{batch_idx}.pt")
+        torch.save(batch, save_path)
+
+
 if __name__ == '__main__':
     args = get_parser()
     now = datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
@@ -55,6 +66,9 @@ if __name__ == '__main__':
     
     ckptdir = os.path.join(logdir, "checkpoints")
     os.makedirs(ckptdir, exist_ok=True)
+
+    inputdir = os.path.join(logdir, "inputs")
+    os.makedirs(inputdir, exist_ok=True)
 
     model : Transformer = instantiate_from_config(cfg.model)
     if 'ckpt_path' in cfg.model:
@@ -84,6 +98,41 @@ if __name__ == '__main__':
         )
 
         trainer.fit(model, train_dataloaders=dataloader)
+
+    if cfg.hijack:
+        # train input to hijack model and produce bad (what we want unlearned) output
+        # train model to unlearn bad tasks
+        # see how model performs on hijacked data?
+
+        # train model + save training data
+        bs, base_lr = cfg.train.batch_sz, cfg.train.lr
+        model.learning_rate = base_lr
+        dataloader = DataLoader(data, batch_size=cfg.train.batch_sz)
+        lightning_cfg = cfg.train.lightning_cfg if 'lightning_cfg' in cfg.train else {}
+        
+        trainer = pl.Trainer(
+            logger=TensorBoardLogger(save_dir=logdir),
+            callbacks=[
+                ModelCheckpoint(
+                    dirpath=os.path.join(ckptdir, 'trainstep_checkpoints'),
+                    verbose=True,
+                    every_n_epochs=5,
+                    save_weights_only=True
+                ),
+                SaveInputCallback(out_dir=inputdir)
+            ],
+            **lightning_cfg
+        )
+
+        trainer.fit(model, train_dataloaders=dataloader)
+
+        # 
+
+        for param in model.parameters():
+            param.requires_grad = False
+        
+        input.requires_grad = True
+
 
 
 
